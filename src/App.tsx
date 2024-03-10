22a53d1486e354f9bd5c76aa0d6f7069c015781b
YllanSelme <yllanselme@gmail.com>
2024-03-10 17:03:55 +0100

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home";
+import Home from "./Home/Home";
 import Installation from "./docs/Installation/Installation";
 
 function App() {
--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,4 +1,4 @@
-import Header from "./Home/components/Header";
+import Header from "./components/Header";
 import { Link } from "react-router-dom";
 
 function Home() {
